9dd744d7eb2c4134a3d2f1b0fff5d3e9116fd7c3
JungHyunCha <wlrqjs@naver.com>
2021-02-22 17:05:25 +0900

    3장 완료

---
+++ chap03/Chap03App/VarTestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VarTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 100.5f;
+            Console.WriteLine($"정수값은 {i}입니다.");
+        }
+    }
+}
